72552cfd549b3cc8fabffe5b1ecb50340fc1e162
Michael Kennedy <mikeckennedy@gmail.com>
2025-11-30 14:15:42 -0800

    ruff format for some of the files.

--- projects/funny-web/app.py
+++ projects/funny-web/app.py
@@ -7,12 +7,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.get("/")
+@app.get('/')
 def tell_a_joke():
-    print("Beginning to tell a joke!")
+    print('Beginning to tell a joke!')
     joke = random.choice(data.jokes)
-    print(f"The joke we are telling is {joke}")
+    print(f'The joke we are telling is {joke}')
     return flask.render_template('joke.html', joke_text=joke)
+
 
 if __name__ == '__main__':
     app.run(port=8001, debug=True)
--- projects/funny-web/data.py
+++ projects/funny-web/data.py
@@ -1,5 +1,5 @@
 jokes = [
     "Relationship status? I'll leave the relations to the database.",
-    "How do you get the code for the bank vault? You checkout their branch.",
-    "What diet did the ghost developer go on? Boolean",
+    'How do you get the code for the bank vault? You checkout their branch.',
+    'What diet did the ghost developer go on? Boolean',
 ]
